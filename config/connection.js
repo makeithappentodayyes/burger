cb948ef4572ec0a4683d10efc9ab1f6068202a76
Daniel Gil <danielgilucsd@gmail.com>
2020-01-16 03:20:57 -0800

    created my sql connection

---
+++ config/connection.js
@@ -0,0 +1,17 @@
+
+
+const mysql = require("mysql");
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "1234",
+    database: "burgers_db"
+  });
+
+}
+
+connection.connect();
+module.exports = connection;
